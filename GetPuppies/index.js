67b7c59db5713e8bd90500fda344a67c97649986
Simona Cotin <simona.cotin@gmail.com>
2018-05-07 04:19:03 -0700

    Use cosmosdb bindings

--- GetPuppies/index.js
+++ GetPuppies/index.js
@@ -5,27 +5,9 @@ const auth = {
 };
 module.exports = function(context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
-  MongoClient.connect(
-    process.env.CosmosDBURL,
-    { auth: auth },
-    (err, database) => {
-      if (err) throw err;
-      console.log('Connected succesfully');
-      const db = database.db(process.env.CosmosDB);
-      db
-        .collection('Puppies')
-        .find()
-        .toArray((err, result) => {
-          if (err) throw err;
-          console.log('retrieved succesfully');
-          result.forEach(puppy => delete puppy._id);
-          context.res = {
-            status: 200,
-            body: result
-          };
-          database.close();
-          context.done();
-        });
-    }
-  );
+
+  context.res = {
+    status: 200,
+    body: context.bindings.puppies
+  };
 };
