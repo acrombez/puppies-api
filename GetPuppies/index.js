18d07a1feb99ba322928fd706bbc59d8d20af20f
Simona Cotin <simona.cotin@gmail.com>
2018-05-07 21:10:05 -0700

    test binding

--- GetPuppies/index.js
+++ GetPuppies/index.js
@@ -1,28 +1,10 @@
-const MongoClient = require('mongodb').MongoClient;
-const URL = 'mongodb://build-serverless.documents.azure.com:10255/?ssl=true';
-
 module.exports = function(context, req) {
-  const auth = {
-    user: process.env.CosmosDBUser,
-    password: process.env.CosmosDBPass
+  context.log('JavaScript HTTP trigger function processed a request.');
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: context.bindings.puppies
   };
-  MongoClient.connect(
-    process.env.CosmosDBURL,
-    { auth: auth },
-    (err, database) => {
-      if (err) throw err;
-      const db = database.db('admin');
-      db
-        .collection('Puppies')
-        .find()
-        .toArray((err, result) => {
-          if (err) throw err;
-          context.res = {
-            body: result
-          };
-          database.close();
-          context.done();
-        });
-    }
-  );
+
+  context.done();
 };
